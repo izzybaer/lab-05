beb4fd01bf86b64c6ff2ac2d75c1c6aaa6b91221
Izabella Arya <izabellabaer1@Izabella-Baers-MacBook-Pro.local>
2017-04-07 18:37:59 -0700

    problem5 solved

--- app.js
+++ app.js
@@ -109,12 +109,25 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
-// Write your code here
-function multiplyArray(testArray){ //eslint-disable-line
+// write a function called multiplyArray() that passes in an array of numbers as its argument
+// the function will return an array
+// the first element of the returned array is the product of the numbers in the array
+// the second element in the returned array is a string
+// the string uses the values that were input into the function, and follows this example The numbers 2,3,4 have a product of 24.
+// use your sum function to do addition
 
+function multiplyArray(testArray){
+  var start = 1;//eslint-disable-line
+  for(var i = 0; i < testArray.length; i++) {
+    start = multiply(start,testArray[i])[0];
+  }
+  var second = ('The numbers ' + testArray + ' have a product of ' + start + '.');
+  return [start,second];
 }
 
+// console.log(multiplyArray(testArray));
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
